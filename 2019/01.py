86d5fb16a7bd9743c9e17a5e55cf1b3bfab1f4bc
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-12-03 15:50:33 -0800

    Create 01.py

---
+++ 2019/01.py
@@ -0,0 +1,19 @@
+
+def fuel_needed(i):
+    return  (i // 3) - 2
+
+def part1(data):
+    r = 0
+    for i in data.split("\n"):
+        r += fuel_needed(int(i))
+    return r
+
+def part2(data):
+    r = 0
+    for i in data.split("\n"):
+        i = int(i)
+        needed =  fuel_needed(i)
+        while needed > 0:
+            r += needed
+            needed = fuel_needed(needed)
+    return r
