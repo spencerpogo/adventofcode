baf46c6d6e494601b5dce5e9ce1283eaa23b42da
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-12-18 16:49:14 -0800

    Quote stuff

--- python.sh
+++ python.sh
@@ -7,9 +7,9 @@ if [[ $# -lt 2 ]]; then
     exit 1
 fi
 
-year=$1
-day=$2
-repo=$(dirname $(realpath "$0"))
+year="$1"
+day="$2"
+repo="$(dirname "$(realpath "$0")")"
 
 PYTHONPATH="$repo/$year${PYTHONPATH:+:PYTHONPATH}" python -c \
     'import sys; from runner import run; run(year=int(sys.argv[1]), day=int(sys.argv[2]))' \
